64c345218cf95680b2d988aa5122280fd77098eb
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-02-02 17:23:56 -0300

    first commit

---
+++ src/styles/globalStyle.tsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Poppins', sans-serif;
+    font-size: 16px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  *, *:after, *:before {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
