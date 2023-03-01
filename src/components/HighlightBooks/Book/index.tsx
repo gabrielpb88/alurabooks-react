06638f7aec54273f902904efb42e719a30614fd8
Gabriel Pereira Bastos <gabrielpb88@gmail.com>
2023-03-01 13:11:16 -0300

    fix(boook): query author to display at book card

--- src/components/HighlightBooks/Book/index.tsx
+++ src/components/HighlightBooks/Book/index.tsx
@@ -2,28 +2,34 @@ import { ButtonWrapper, CardDescription, CardEm, CardPrice, CardTitle, Footer, H
 import { AbButton } from 'alurabooks-ds-gb';
 import { IBook } from '../../../types';
 import { getLowestPrice } from 'utils';
+import { useQuery } from 'react-query';
+import { getAuthor } from '../../../http';
 
 export interface BookProps {
   book: IBook;
 }
 
-const Book = ({ book }: BookProps) => (
-  <>
-    <H4>Sobre o livro:</H4>
-    <CardTitle>{book.titulo}</CardTitle>
-    <CardDescription>{book.descricao}</CardDescription>
-    <CardDescription>Por: {book.autor}</CardDescription>
-    <Footer>
-      <div>
-        <CardEm>A partir de:</CardEm>
-        <CardPrice>
-          {Intl.NumberFormat('pt-br', { style: 'currency', currency: 'brl' }).format(getLowestPrice(book))}
-        </CardPrice>
-      </div>
-      <ButtonWrapper>
-        <AbButton>Comprar</AbButton>
-      </ButtonWrapper>
-    </Footer>
-  </>
-);
+const Book = ({ book }: BookProps) => {
+  const { data: author } = useQuery(['getAuthor', book.autor], () => getAuthor(book.autor));
+
+  return (
+    <>
+      <H4>Sobre o livro:</H4>
+      <CardTitle>{book.titulo}</CardTitle>
+      <CardDescription>{book.descricao}</CardDescription>
+      <CardDescription>Por: {author && author.nome}</CardDescription>
+      <Footer>
+        <div>
+          <CardEm>A partir de:</CardEm>
+          <CardPrice>
+            {Intl.NumberFormat('pt-br', { style: 'currency', currency: 'brl' }).format(getLowestPrice(book))}
+          </CardPrice>
+        </div>
+        <ButtonWrapper>
+          <AbButton>Comprar</AbButton>
+        </ButtonWrapper>
+      </Footer>
+    </>
+  );
+};
 export default Book;
